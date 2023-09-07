84a85927bb27b90565c3e1f87f7b20c3eeea7c6a
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-09-08 07:28:54 +0930

    update

--- src/Pterodactyl.php
+++ src/Pterodactyl.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Artichoke\Pterodactyl;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Collection;
 
 class Pterodactyl
 {
@@ -11,6 +13,30 @@ class Pterodactyl
     public static function init() {
         self::$api_key = config('pterodactyl.api_key');
         self::$api_url = config('pterodactyl.api_url');
+    }
+
+    public static function users() {
+        $data = self::get('/users');
+        $users = [];
+        foreach ($data['data'] as $userData) {
+            $user = new User();
+            $user->fromApiData($userData['attributes']);
+            $users[] = $user;
+        }
+
+        return new Collection($users);
+
+    }
+
+    public static function get($endpoint) {
+        $url = self::$api_url.'/api/application'.$endpoint;
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer '.self::$api_key,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ])->get($url);
+        return $response->json();
+
     }
 
 }
---
+++ src/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Artichoke\Pterodactyl;
+
+class User
+{
+
+    public $id;
+    public $external_id;
+    public $uuid;
+    public $username;
+    public $email;
+    public $first_name;
+    public $last_name;
+    public $language;
+    public $root_admin;
+    public $multi_factor_auth;
+    public $created_at;
+    public $updated_at;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function fromApiData(array $data)
+    {
+        $this->id = $data['id'];
+        $this->external_id = $data['external_id'];
+        $this->uuid = $data['uuid'];
+        $this->username = $data['username'];
+        $this->email = $data['email'];
+        $this->first_name = $data['first_name'];
+        $this->last_name = $data['last_name'];
+        $this->language = $data['language'];
+        $this->root_admin = $data['root_admin'];
+        $this->multi_factor_auth = $data['2fa'];
+        $this->created_at = $data['created_at'];
+        $this->updated_at = $data['updated_at'];
+    }
+
+}
